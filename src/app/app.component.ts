4ae87613d9ea91035aa2b8c199a1da50e6bf0ee1
Elżbieta Obara <elzbieta.obara@kruksa.pl>
2025-10-02 08:40:43 +0200

    dodanie Zadan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,          // <-- ważne
+  imports: [RouterOutlet, CommonModule], // <-- ważne
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
